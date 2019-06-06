890c16a7033714939f05f86e5ab0c7b7687d3023
Serhiy Savchuk and Wei Kuang <serhiy@>
2019-06-06 11:02:44 -0700

    Add hide functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,23 @@
 
 
 $(document).ready(function(){
-  
+
   $( "input" ).on( "click", function(event) {
 
     var animal = $("input:checked").val();
 
       if (animal==="turtle"){
         $(".turtles").show();
+        $(".snakes").hide();
+        $(".insects").hide();
       } else if (animal==="snake"){
         $(".snakes").show();
+        $(".turtles").hide();
+        $(".insects").hide();
       } else {
         $(".insects").show();
+        $(".snakes").hide();
+        $(".turtles").hide();
       }
 
     event.preventDefault();
